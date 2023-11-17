624dc25db8843547c7f810b754597b3b1402846d
nggabbo <g4briel.ruiz@gmail.com>
2023-11-17 18:03:21 -0300

    build: :wrench: migrate to esbuild/vite builder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fictional.cinema.app',
   appName: 'fictional-cinema-app',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
