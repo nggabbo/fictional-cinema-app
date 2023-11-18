42b2e499f714941ca9e6b56b47b1ab32edd7dc57
nggabbo <g4briel.ruiz@gmail.com>
2023-11-17 21:58:06 -0300

    feat: :sparkles: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
   },
   {
-    path: '',
-    redirectTo: 'home',
+    path: '**',
+    redirectTo: 'auth',
     pathMatch: 'full',
   },
 ];
