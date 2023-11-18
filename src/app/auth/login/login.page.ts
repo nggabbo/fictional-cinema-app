e52dcf4245505590f3f9617f21521cc93c0a5750
nggabbo <g4briel.ruiz@gmail.com>
2023-11-17 21:59:11 -0300

    feat: :sparkles: create first login page version

---
+++ src/app/auth/login/login.page.ts
@@ -0,0 +1,33 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, ReactiveFormsModule, NgStyle],
+})
+export default class LoginPage {
+  loginForm = new FormGroup({
+    username: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
+
+  uploadAvatarPhoto(): void {
+    
+  }
+}
