056d45dc758a6e0df337001c36680d592e22a82e
nggabbo <g4briel.ruiz@gmail.com>
2023-11-20 15:53:57 -0300

    feat: :sparkles: make movies list, iterate movie card component

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Movie } from '@shared/models';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [MovieCardComponent],
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.scss'],
 })
-export default class MoviesListComponent {}
+export default class MoviesListComponent {
+  movies: Movie[] = [
+    {
+      uuid: '',
+      title: 'Title test',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum interdum nisi odio, eget commodo nunc condimentum eget. Ut facilisis metus scelerisque lectus tincidunt luctus.',
+      shortDescription:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum interdum nisi odio, eget commodo nunc condimentum eget. Ut facilisis metus scelerisque lectus tincidunt luctus.',
+      rate: 3,
+      releaseDate: '',
+      genre: '',
+      image: { filepath: '', webviewPath: '' },
+    },
+  ];
+}
