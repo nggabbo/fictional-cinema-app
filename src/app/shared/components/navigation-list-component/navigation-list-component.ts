91c9526f6d6e70a281a6725e5ec2d1783f12ee2f
nggabbo <g4briel.ruiz@gmail.com>
2023-11-20 20:12:23 -0300

    feat: :sparkles: toggle menu on option clicked

--- src/app/shared/components/navigation-list-component/navigation-list-component.ts
+++ src/app/shared/components/navigation-list-component/navigation-list-component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonIcon, IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
+import { IonIcon, IonItem, IonLabel, IonList, IonMenuToggle } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { videocam } from 'ionicons/icons';
 
 @Component({
   standalone: true,
-  imports: [IonList, IonItem, IonLabel, IonIcon, RouterLink],
+  imports: [IonList, IonItem, IonLabel, IonIcon, IonMenuToggle, RouterLink],
   selector: 'app-navigation-list-component',
   templateUrl: './navigation-list-component.html',
   styleUrls: ['./navigation-list-component.scss'],
