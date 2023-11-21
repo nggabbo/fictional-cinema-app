cf23881dfc8122aacbb5621750e702b8635df5fc
nggabbo <g4briel.ruiz@gmail.com>
2023-11-21 16:37:40 -0300

    chore: :bento: update splash screen and app icon, resize icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'fictional.cinema.app',
-  appName: 'fictional-cinema-app',
+  appName: 'Fictional Cinema',
   webDir: 'www/browser',
   server: {
     androidScheme: 'https',
